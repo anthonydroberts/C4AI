f85ca9d3d6e53e46bb5e535ad4606591b1e0cd8e
Tony Roberts <123ananan@gmail.com>
2019-03-11 01:06:06 -0400

    score checking, draw checking, next is minimax,AB

--- c4Game.js
+++ c4Game.js
@@ -61,7 +61,6 @@ function drawBoard(){
 
 
 //check for a win or a draw by scanning board
-//TODO: check for draw
 function checkBoard(){
 	let HEIGHT = board.length;
     let WIDTH = board[0].length;
@@ -95,7 +94,236 @@ function checkBoard(){
             }
         }
     }
+	//no win, check for draw
+	for(let i = 0; i < board.length; i++){
+		for(let j = 0; j < board[i].length; j++){
+			if(board[i][j] == 0){
+				return 0;
+			}
+		}
+	}
+	//draw check did not detect any empty grids, game is a draw return draw
+	console.log("DRAW GAME");
 	return 0;
+}
+
+// takes in a new board state and returns its evaluated score 
+function getScore(b, p){
+	let SCORE = 0;
+	let HEIGHT = b.length;
+    let WIDTH = b[0].length;
+	let player1 = -1;
+	let player2 = -2;
+	
+	if(p == 1){
+		player1 = 1;
+		player2 = 2;
+	}
+	else if(p == 2){
+		player1 = 2;
+		player2 = 1;
+	}
+	
+    for (let r = 0; r < HEIGHT; r++) { // iterate rows, bottom to top
+        for (let c = 0; c < WIDTH; c++) { // iterate columns, left to right
+            if (b[r][c] == 0)
+                continue; // don't check empty slots
+			else if (b[r][c] == player1){ // we are checking a computers tile
+				if(c == 3 && player1 == b[r][c]){ //middle column pieces are worth more
+					SCORE = SCORE + 2;
+				}
+				
+				if (c + 3 < WIDTH &&
+					player1 == b[r][c+1] && // look right
+					player1 == b[r][c+2] &&
+					player1 == b[r][c+3]){
+							//player1 4 in a row horizontally
+						SCORE = SCORE + 10000;
+					}
+					
+				if (c + 2 < WIDTH &&
+					player1 == b[r][c+1] && // look right
+					player1 == b[r][c+2]){
+							//player1 3 in a row horizontally
+						SCORE = SCORE + 5;
+					}
+				
+				if (c + 1 < WIDTH &&
+					player1 == b[r][c+1]){
+							//player1 2 in a row horizontally
+						SCORE = SCORE + 2;
+					}
+				
+				if (r + 3 < HEIGHT &&
+					player1 == b[r+1][c] && // look up
+					player1 == b[r+2][c] &&
+					player1 == b[r+3][c]){
+						//player1 4 in a row vertically
+						SCORE = SCORE + 10000;
+					}
+
+				if (r + 2 < HEIGHT &&
+					player1 == b[r+1][c] && // look up
+					player1 == b[r+2][c]){
+						//player1 3 in a row vertically
+						SCORE = SCORE + 5;
+					}
+					
+				if (r + 1 < HEIGHT &&
+					player1 == b[r+1][c]){
+						//player1 2 in a row vertically
+						SCORE = SCORE + 2;
+					}
+					
+				if (r + 3 < HEIGHT &&
+					c + 3 < WIDTH &&
+					player1 == b[r+1][c+1] && // look up & right
+					player1 == b[r+2][c+2] &&
+					player1 == b[r+3][c+3]){
+						//player1 4 in a row diagonally
+						SCORE = SCORE + 10000;
+					}
+					
+				if (r + 2 < HEIGHT &&
+					c + 2 < WIDTH &&
+					player1 == b[r+1][c+1] && // look up & right
+					player1 == b[r+2][c+2]){
+						//player1 3 in a row diagonally
+						SCORE = SCORE + 5;
+					}
+					
+				if (r + 1 < HEIGHT &&
+					c + 1 < WIDTH &&
+					player1 == b[r+1][c+1]){
+						//player1 2 in a row diagonally
+						SCORE = SCORE + 2;
+					}
+					
+				if (r + 3 < HEIGHT &&
+					c - 3 >= 0 &&
+					player1 == b[r+1][c-1] && // look up & left
+					player1 == b[r+2][c-2] &&
+					player1 == b[r+3][c-3]){
+						//player1 4 in a row diagonally
+						SCORE = SCORE + 10000;
+					}
+					
+				if (r + 2 < HEIGHT &&
+					c - 2 >= 0 &&
+					player1 == b[r+1][c-1] && // look up & left
+					player1 == b[r+2][c-2]){
+						//player1 3 in a row diagonally
+						SCORE = SCORE + 5;
+					}
+
+				if (r + 1 < HEIGHT &&
+					c - 1 >= 0 &&
+					player1 == b[r+1][c-1]){
+						//player1 2 in a row diagonally
+						SCORE = SCORE + 2;
+					}
+						
+			}
+			else if(b[r][c] == player2){  // we are checking a players tile
+				if(c == 3 && player2 == b[r][c]){ //middle column pieces are worth more
+					SCORE = SCORE - 2;
+				}
+			
+				if (c + 3 < WIDTH &&
+					player2 == b[r][c+1] && // look right
+					player2 == b[r][c+2] &&
+					player2 == b[r][c+3]){
+							//player2 4 in a row horizontally
+						SCORE = SCORE - 10000;
+					}
+					
+				if (c + 2 < WIDTH &&
+					player2 == b[r][c+1] && // look right
+					player2 == b[r][c+2]){
+							//player2 3 in a row horizontally
+						SCORE = SCORE - 5;
+					}
+				
+				if (c + 1 < WIDTH &&
+					player2 == b[r][c+1]){
+							//player2 2 in a row horizontally
+						SCORE = SCORE - 2;
+					}
+				
+				if (r + 3 < HEIGHT &&
+					player2 == b[r+1][c] && // look up
+					player2 == b[r+2][c] &&
+					player2 == b[r+3][c]){
+						//player2 4 in a row vertically
+						SCORE = SCORE - 10000;
+					}
+
+				if (r + 2 < HEIGHT &&
+					player2 == b[r+1][c] && // look up
+					player2 == b[r+2][c]){
+						//player2 3 in a row vertically
+						SCORE = SCORE - 5;
+					}
+					
+				if (r + 1 < HEIGHT &&
+					player2 == b[r+1][c]){
+						//player2 2 in a row vertically
+						SCORE = SCORE - 2;
+					}
+					
+				if (r + 3 < HEIGHT &&
+					c + 3 < WIDTH &&
+					player2 == b[r+1][c+1] && // look up & right
+					player2 == b[r+2][c+2] &&
+					player2 == b[r+3][c+3]){
+						//player2 4 in a row diagonally
+						SCORE = SCORE - 10000;
+					}
+					
+				if (r + 2 < HEIGHT &&
+					c + 2 < WIDTH &&
+					player2 == b[r+1][c+1] && // look up & right
+					player2 == b[r+2][c+2]){
+						//player2 3 in a row diagonally
+						SCORE = SCORE - 5;
+					}
+					
+				if (r + 1 < HEIGHT &&
+					c + 1 < WIDTH &&
+					player2 == b[r+1][c+1]){
+						//player2 2 in a row diagonally
+						SCORE = SCORE - 2;
+					}
+					
+				if (r + 3 < HEIGHT &&
+					c - 3 >= 0 &&
+					player2 == b[r+1][c-1] && // look up & left
+					player2 == b[r+2][c-2] &&
+					player2 == b[r+3][c-3]){
+						//player2 4 in a row diagonally
+						SCORE = SCORE - 10000;
+					}
+					
+				if (r + 2 < HEIGHT &&
+					c - 2 >= 0 &&
+					player2 == b[r+1][c-1] && // look up & left
+					player2 == b[r+2][c-2]){
+						//player2 3 in a row diagonally
+						SCORE = SCORE - 5;
+					}
+
+				if (r + 1 < HEIGHT &&
+					c - 1 >= 0 &&
+					player2 == b[r+1][c-1]){
+						//player2 2 in a row diagonally
+						SCORE = SCORE - 2;
+					}
+			}
+
+        }
+    }
+	
+	return SCORE;
 }
 
 //function to set all relevant variables, also used for resetting game
@@ -130,6 +358,7 @@ $('#board td').click(function(){
 				playerTurn = 1;
 			}
 		}
+		console.log(getScore(board,1))
 	}
 	
 });
