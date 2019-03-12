4398fc76ed11ec4066a8146a6872e9767d5b3231
Tony Roberts <123ananan@gmail.com>
2019-03-12 13:08:21 -0400

    finishing touches

--- c4Game.js
+++ c4Game.js
@@ -10,6 +10,9 @@ var playerTurn = 1; //1 for player 1, or 2 for player 2
 var depthLimit = 0;
 var statisticsString = "";
 var p1Wins = 0; var p2Wins = 0;
+var gameOverFlag = 0; //flag for whether the game has ended, 0 means it is active, 1 means it needs to be reset
+var newAIMove = [-1,-1]; //holds newest AI decision, row,col
+var nodesSearched = 0; //global tracks nodes searched for minimax
 
 //update board grid values, x position, y position, player number (1 or 2)			 
 function updateBoardValues(x,y,p){
@@ -78,6 +81,9 @@ function drawBoard(){
 				let cell = $("tr:eq(" + i + ")").find('td').eq(j).css('background', '#ff3c64');
 			}
 			else if(board[i][j] == 2){
+				let cell = $("tr:eq(" + i + ")").find('td').eq(j).css('background', '#3062D9');
+			}
+			if(i == newAIMove[0] && j == newAIMove[1]){ //newest ai move, color it different
 				let cell = $("tr:eq(" + i + ")").find('td').eq(j).css('background', 'blue');
 			}
 		}
@@ -296,7 +302,7 @@ function getScore(b, p){
 					player2 == b[r][c+1] && // look right
 					player2 == b[r][c+2]){
 							//player2 3 in a row horizontally
-						SCORE = SCORE - 5;
+						SCORE = SCORE - 500;
 					}
 				
 				if (c + 1 < WIDTH &&
@@ -317,7 +323,7 @@ function getScore(b, p){
 					player2 == b[r+1][c] && // look up
 					player2 == b[r+2][c]){
 						//player2 3 in a row vertically
-						SCORE = SCORE - 5;
+						SCORE = SCORE - 500;
 					}
 					
 				if (r + 1 < HEIGHT &&
@@ -340,7 +346,7 @@ function getScore(b, p){
 					player2 == b[r+1][c+1] && // look up & right
 					player2 == b[r+2][c+2]){
 						//player2 3 in a row diagonally
-						SCORE = SCORE - 5;
+						SCORE = SCORE - 500;
 					}
 					
 				if (r + 1 < HEIGHT &&
@@ -364,7 +370,7 @@ function getScore(b, p){
 					player2 == b[r+1][c-1] && // look up & left
 					player2 == b[r+2][c-2]){
 						//player2 3 in a row diagonally
-						SCORE = SCORE - 5;
+						SCORE = SCORE - 500;
 					}
 
 				if (r + 1 < HEIGHT &&
@@ -390,14 +396,26 @@ function Node(state, score, depth, par) {
 	this.children = [];
 }
 
+
+//define all values, learn them and track them through the algorithm
 // minimax algorithm, takes current board (game)
 function MINIMAXDECISION(){
 	let b = JSON.parse(JSON.stringify(board));
 	let root = new Node (b, 0, 0, null);
+	nodesSearched = 0;
 	let v = MAXVALUE(root, -Infinity, Infinity);
-	
 	for(let i = 0; i < root.children.length; i++){
 		if(root.children[i].score == v){
+			for(let k = 0; k < board.length; k++){
+				for(let q = 0; q < board[k].length; q++){
+					if(board[k][q] != root.children[i].state[k][q]){
+						console.log("NODES SEARCHED FOR THIS AI MOVE: " + nodesSearched);
+						document.getElementById("newAIMoveString").style.visibility = "visible";
+						document.getElementById("newAIMoveString").innerHTML = "AI played Row: " + k + " Col: " + q;
+						newAIMove = [k,q];
+					}
+				}
+			}
 			return root.children[i].state;
 		}
 	}
@@ -414,6 +432,7 @@ function MAXVALUE(node, alpha, beta){
 	}
 	for(let c = 0; c < 7; c++){
 		if(b[0][c] == 0){ 
+			nodesSearched = nodesSearched + 1;
 			let newState = computerDropPiece(b,c,2);
 			let newScore = 0;
 			let newNode = new Node(newState, newScore, node.depth + 1, node);
@@ -433,6 +452,7 @@ function MAXVALUE(node, alpha, beta){
 	if(gameType == 1){
 		for(let c = 0; c < 7; c++){
 			if(b[5][c] == 2){ 
+				nodesSearched = nodesSearched + 1;
 				let newState2 = computerRemovePiece(b,c,2);
 				let newScore2 = 0;
 				let newNode2 = new Node(newState2, newScore2, node.depth + 1, node);
@@ -462,6 +482,7 @@ function MINVALUE(node, alpha, beta){
 	}
 	for(let c = 0; c < 7; c++){
 		if(b[0][c] == 0){ 
+			nodesSearched = nodesSearched + 1;
 			let newState = computerDropPiece(b,c,1);
 			let newScore = 0;
 			let newNode = new Node(newState, newScore, node.depth + 1, node);
@@ -480,6 +501,7 @@ function MINVALUE(node, alpha, beta){
 	if(gameType == 1){
 		for(let c = 0; c < 7; c++){
 			if(b[5][c] == 2){ 
+				nodesSearched = nodesSearched + 1;
 				let newState2 = computerRemovePiece(b,c,2);
 				let newScore2 = 0;
 				let newNode2 = new Node(newState2, newScore2, node.depth + 1, node);
@@ -502,20 +524,30 @@ function MINVALUE(node, alpha, beta){
 //implements dumb ai
 function randomMove(){
 	if (gameType == 1){
-		if (Math.floor((Math.random() * 3) + 1) == 1){ // 1 in 3 chance of attempting to removing piece
-			let c = Math.floor((Math.random() * 6) + 0);
-			if (board[5][c] == 2){
-				for(let i = 5; i > 0; i--){
-					board[i][c] = board[i-1][c];
+		if (Math.floor((Math.random() * 2) + 1) == 1){ // 1 in 3 chance of attempting to removing piece
+			console.log("ATTEMPTING A REMOVAL");
+			for(let k = 0; k < board[5].length;k++){//make sure there is a piece to remove
+				if(board[5][k] == 1){
+					console.log("CORRECT PIECE DETECTED");
+					while(1){
+						let c = Math.floor((Math.random() * 6) + 0);
+						if (board[5][c] == 1){
+							for(let i = 5; i > 0; i--){
+								board[i][c] = board[i-1][c];
+							}
+							board[0][c] = 0;
+							console.log("REMOVAL SUCCESS AT COL: " + c);
+							return 1;
+						}
+					}
 				}
-				board[0][c] = 0;
-				return 1;
 			}
+
 		}
 		//if we failed removing in some way, we will attempt to randomly place until we succeed
 		while(1){
 			let c = Math.floor((Math.random() * 6) + 0);
-			if(board[5][c] == 0){
+			if(board[0][c] == 0){
 				for(let i = 5; i >= 0; i--){
 					if (board[i][c] == 0){
 						board[i][c] = 1;
@@ -529,8 +561,9 @@ function randomMove(){
 		//we will attempt to randomly place until we succeed
 		while(1){
 			let c = Math.floor((Math.random() * 6) + 0);
-			if(board[5][c] == 0){
+			if(board[0][c] == 0){
 				for(let i = 5; i >= 0; i--){
+					console.log("ATTEMPTING ROW " + i + " COL: " + c);
 					if (board[i][c] == 0){
 						board[i][c] = 1;
 						return 1;
@@ -541,6 +574,12 @@ function randomMove(){
 	}
 }
 
+function endHandler(string){
+	document.getElementById("endString").style.visibility = "visible";
+	document.getElementById("endString").innerHTML = string;
+	gameOverFlag = 1;
+}
+
 //function to set all relevant variables, also used for resetting game
 function init(gt,pt,depth){
 	board = [[0,0,0,0,0,0,0],
@@ -549,68 +588,84 @@ function init(gt,pt,depth){
              [0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0]];
+	newAIMove = [-1,-1];
 	document.getElementById("board").style.visibility = "visible";
+	document.getElementById("newAIMoveString").style.visibility = "hidden";
+	document.getElementById("endString").style.visibility = "hidden";
 	document.getElementById("initGame").innerHTML = "Restart";
+	let gameString = "";
+	if(gt == 0){gameString = "Classic";}
+	else{gameString = "Alternative";}
+	if(pt == 1){gameString = gameString + " Human(Red) vs AI(Blue)";}
+	else if(pt == 2){gameString = gameString + " Dumb AI(Red) vs AI(Blue) (Click board to execute turns)";}
+	else if(pt == 0){gameString = gameString + " Player 1(Red) vs Player 2(Blue)";}
+	document.getElementById("currentGame").innerHTML = gameString;
 	gameType = gt; //game type refers to 0 for standard C4, or 1 for alternate dropping version
 	playType = pt; //0 for human vs human, 1 for human vs ai, 2 for ai vs ai
 	playerTurn = 1; //1 for player 1, or 2 for player 2
 	depthLimit = depth; //maximum depth to search ahead for AI
 	statisticsString = "";
 	drawBoard();
-	$('#board td').click(tableClick);
+	gameOverFlag = 0;
+	
 }
 
 //click handler for grid positions when player clicks
-
+$('#board td').click(tableClick);
 function tableClick(){
-	
-	if (playType == 0 || playType == 1){
-		let y = $('#board tr').index($(this).closest('tr'));
-		let x = $(this).closest('tr').find('td').index($(this).closest('td'));
-		
-		//check if updateBoardValues returns success then proceed
-		if(updateBoardValues(x,y, playerTurn) == 1){
-			drawBoard()
-			let checkBoardRet = checkBoard();
-			if (checkBoardRet == 1){alert("Player " + playerTurn + " wins!"); $('#board td').unbind('click'); return 0;}//player wins
-			else if(checkBoardRet == 3){alert("Draw!"); $('#board td').unbind('click'); return 0;}//draw
-			if(playType == 0){ //human vs human, switch turns
-				if (playerTurn == 1){
-					playerTurn = 2;
-				}
-				else if(playerTurn == 2){
-					playerTurn = 1;
-				}
-			}
-			else if(playType == 1){
-				board = MINIMAXDECISION(board);
+	if(gameOverFlag == 0){
+		if (playType == 0 || playType == 1){
+			let y = $('#board tr').index($(this).closest('tr'));
+			let x = $(this).closest('tr').find('td').index($(this).closest('td'));
+			
+			//check if updateBoardValues returns success then proceed
+			if(updateBoardValues(x,y, playerTurn) == 1){
 				drawBoard()
 				let checkBoardRet = checkBoard();
-				if (checkBoardRet == 2){alert("AI wins!"); $('#board td').unbind('click'); return 0;}//AI wins
-				else if(checkBoardRet == 3){alert("Draw!"); $('#board td').unbind('click'); return 0;}//draw
+				if (checkBoardRet == 1 || checkBoardRet == 2){endHandler("Player " + playerTurn + " wins!"); return 0;}//player wins
+				else if(checkBoardRet == 3){endHandler("Draw!"); return 0;}//draw
+				if(playType == 0){ //human vs human, switch turns
+					if (playerTurn == 1){
+						playerTurn = 2;
+					}
+					else if(playerTurn == 2){
+						playerTurn = 1;
+					}
+				}
+				else if(playType == 1){
+					board = MINIMAXDECISION(board);
+					drawBoard();
+					let checkBoardRet = checkBoard();
+					if (checkBoardRet == 2){endHandler("AI wins!"); return 0;}//AI wins
+					else if(checkBoardRet == 3){endHandler("Draw!"); return 0;}//draw
+				}
+			}
+		}
+		
+		else if (playType == 2){ //dumb ai vs ai
+			if(playerTurn == 1){
+				randomMove();
+				drawBoard();
+				let checkBoardRet = checkBoard();
+				if (checkBoardRet == 1){endHandler("Dumb AI wins!"); return 0;}//DUMB AI wins
+				else if(checkBoardRet == 3){endHandler("Draw!"); return 0;}//draw
+				playerTurn = 2;
+			}
+			else if(playerTurn == 2){
+				board = MINIMAXDECISION(board);
+				drawBoard();
+				checkBoardRet = checkBoard();
+				if (checkBoardRet == 2){endHandler("AI wins!"); return 0;}//AI wins
+				else if(checkBoardRet == 3){endHandler("Draw!"); return 0;}//draw
+				playerTurn = 1;
 			}
 		}
 	}
-	
-	else if (playType == 2){ //dumb ai vs ai
-		randomMove();
-		console.log(board);
-		drawBoard();
-		let checkBoardRet = checkBoard();
-		if (checkBoardRet == 1){alert("Dumb AI wins!"); $('#board td').unbind('click'); return 0;}//DUMB AI wins
-		else if(checkBoardRet == 3){alert("Draw!"); $('#board td').unbind('click'); return 0;}//draw
-		board = MINIMAXDECISION(board);
-		console.log(board);
-		drawBoard();
-		checkBoardRet = checkBoard();
-		if (checkBoardRet == 2){alert("AI wins!"); $('#board td').unbind('click'); return 0;}//AI wins
-		else if(checkBoardRet == 3){alert("Draw!"); $('#board td').unbind('click'); return 0;}//draw
-	}
-	
 }
 
 $('#initGame').click(function(){
 	init(document.getElementById("SelectGameType").value,
 	document.getElementById("SelectPlayType").value,
 	document.getElementById("SelectDepth").value);
+	
 });
